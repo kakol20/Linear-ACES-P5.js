c6e034b19b953814dd03f6c408ec483f23ed3751
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2023-10-02 20:43:07 +0100

    Update ImgWrap.js

--- ImgWrap.js
+++ ImgWrap.js
@@ -1,61 +1,62 @@
 const ColorSpace = {
-  sRGB: "sRGB",
-  Linear: "Linear"
+    sRGB: "sRGB",
+    Linear: "Linear"
 }
 
 class ImgWrap {
-  constructor(w, h, colorSpace) {
-    this.img = createImage(w, h);
-    
-    this.width = w;
-    this.height = h;
-    this.size = this.width * this.height * 4;
-    
-    this.data = new Array(this.size);
-    this.colorSpace = colorSpace;
-  }
-  
-  loadPixels(pixels) {
-    this.img.loadPixels();
+    constructor(w, h, colorSpace) {
+        this.img = createImage(w, h);
 
-    // this.data = pixels;
+        this.width = w;
+        this.height = h;
+        this.size = this.width * this.height * 4;
 
-    for (let i in pixels) {
-      this.data[i] = pixels[i] / 255;
-
-      if (this.colorSpace == ColorSpace.sRGB) {
-        this.data[i] = sRGB.toLinear(this.data[i]);
-      }
+        this.data = new Array(this.size);
+        this.colorSpace = colorSpace;
     }
-    
-    this.img.updatePixels();
-  }
-  
-  index(x, y) {
-    return (x + y * this.width) * 4;
-  }
 
-  indexToXY(index) {
-    let pixelIndex = Math.floor(index / 4);
-    let x = pixelIndex % this.width;
-    let y = pixelIndex / this.width;
+    loadPixels(pixels) {
+        this.img.loadPixels();
 
-    return [x, y];
-  }
-  
-  toBW() {
-    for (let x = 0; x < this.width; x++) {
-      for (let y = 0; y < this.height; y++) {
-        const i = this.index(x, y);
-        
-        const l = this.data[i + 0] * 0.2989 +
-              this.data[i + 1] * 0.587 +
-              this.data[i + 2] * 0.114;
-        
-        this.data[i + 0] = l;
-        this.data[i + 1] = l;
-        this.data[i + 2] = l;
-      }
+        // this.data = pixels;
+
+        for (let i in pixels) {
+            this.data[i] = pixels[i] / 255;
+            if (this.colorSpace == ColorSpace.sRGB) {
+                this.data[i] = sRGB.toLinear(this.data[i]);
+            }
+
+            // this.img.pixels[i] = pixels[i];
+        }
+
+        this.img.updatePixels();
     }
-  }
+
+    index(x, y) {
+        return (x + y * this.width) * 4;
+    }
+
+    indexToXY(index) {
+        let pixelIndex = Math.floor(index / 4);
+        let x = pixelIndex % this.width;
+        let y = pixelIndex / this.width;
+
+        return [x, y];
+    }
+
+    toBW() {
+        for (let x = 0; x < this.width; x++) {
+            for (let y = 0; y < this.height; y++) {
+                const i = this.index(x, y);
+
+                const l = this.data[i + 0] * 0.2989 +
+                    this.data[i + 1] * 0.587 +
+                    this.data[i + 2] * 0.114;
+
+                this.data[i + 0] = l;
+                this.data[i + 1] = l;
+                this.data[i + 2] = l;
+            }
+        }
+    }
 }
