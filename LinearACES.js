4d13f962d688969bc0b5b4af69a764b7f6ef5f69
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2023-10-04 23:01:38 +0100

    Change to tab indentation

--- LinearACES.js
+++ LinearACES.js
@@ -1,67 +1,67 @@
 // https://github.com/TheRealMJP/BakingLab/blob/master/BakingLab/ACES.hlsl
-const LinearACES = (function() {
-  const inputMat = function(r, g, b) {
-    let rgb = createVector(r, g, b);
-    
-    let r_m = createVector(0.59719, 0.35458, 0.04823);
-    let g_m = createVector(0.07600, 0.90834, 0.01566);
-    let b_m = createVector(0.02840, 0.13383, 0.83777);
-    
-    return {
-      r: r_m.dot(rgb),
-      g: g_m.dot(rgb),
-      b: b_m.dot(rgb)
-    };
-  }
-  
-  const outputMat = function(r, g, b) {
-    let rgb = createVector(r, g, b);
-    
-    let r_m = createVector( 1.60475, -0.53108, -0.0736);
-    let g_m = createVector(-0.10208,  1.10813, -0.00605);
-    let b_m = createVector(-0.00327, -0.07276,  1.07602);
-    
-    return {
-      r: r_m.dot(rgb),
-      g: g_m.dot(rgb),
-      b: b_m.dot(rgb)
-    };
-  }
-  
-  const RRT_ODT = function(v) {
-    let a = v * (v + 0.0245786) - 0.000090537;
-    let b = v * (0.983729 * v + 0.4329510) + 0.238081;
-    return a / b;
-  }
-  
-  return {
-    ToneMap: function(r, g, b) {
-      
-      const inputM = inputMat(r, g, b);
-      
-      r = inputM.r;
-      g = inputM.g;
-      b = inputM.b;
-      
-      r = RRT_ODT(r);
-      g = RRT_ODT(g);
-      b = RRT_ODT(b);
-      
-      const outputM = outputMat(r, g, b);
-      
-      r = outputM.r;
-      g = outputM.g;
-      b = outputM.b;
-      
-      r = r > 1 ? 1 : r;
-      g = g > 1 ? 1 : g;
-      b = b > 1 ? 1 : b;
-      
-      r = r < 0 ? 0 : r;
-      g = g < 0 ? 0 : g;
-      b = b < 0 ? 0 : b;
-      
-      return [r, g, b];
-    }
-  }
+const LinearACES = (function () {
+	const inputMat = function (r, g, b) {
+		let rgb = createVector(r, g, b);
+
+		let r_m = createVector(0.59719, 0.35458, 0.04823);
+		let g_m = createVector(0.07600, 0.90834, 0.01566);
+		let b_m = createVector(0.02840, 0.13383, 0.83777);
+
+		return {
+			r: r_m.dot(rgb),
+			g: g_m.dot(rgb),
+			b: b_m.dot(rgb)
+		};
+	}
+
+	const outputMat = function (r, g, b) {
+		let rgb = createVector(r, g, b);
+
+		let r_m = createVector(1.60475, -0.53108, -0.0736);
+		let g_m = createVector(-0.10208, 1.10813, -0.00605);
+		let b_m = createVector(-0.00327, -0.07276, 1.07602);
+
+		return {
+			r: r_m.dot(rgb),
+			g: g_m.dot(rgb),
+			b: b_m.dot(rgb)
+		};
+	}
+
+	const RRT_ODT = function (v) {
+		let a = v * (v + 0.0245786) - 0.000090537;
+		let b = v * (0.983729 * v + 0.4329510) + 0.238081;
+		return a / b;
+	}
+
+	return {
+		ToneMap: function (r, g, b) {
+
+			const inputM = inputMat(r, g, b);
+
+			r = inputM.r;
+			g = inputM.g;
+			b = inputM.b;
+
+			r = RRT_ODT(r);
+			g = RRT_ODT(g);
+			b = RRT_ODT(b);
+
+			const outputM = outputMat(r, g, b);
+
+			r = outputM.r;
+			g = outputM.g;
+			b = outputM.b;
+
+			r = r > 1 ? 1 : r;
+			g = g > 1 ? 1 : g;
+			b = b > 1 ? 1 : b;
+
+			r = r < 0 ? 0 : r;
+			g = g < 0 ? 0 : g;
+			b = b < 0 ? 0 : b;
+
+			return [r, g, b];
+		}
+	}
 })();
--- sketch.js
+++ sketch.js
@@ -18,171 +18,171 @@ let restartButton;
 let restarted;
 
 function preload() {
-    input = createFileInput(handleFile);
-    input.position(5, 5);
+	input = createFileInput(handleFile);
+	input.position(5, 5);
 
-    factorText = createSpan("Factor: ");
-    factorInput = createInput(31, "number");
+	factorText = createSpan("Dither Factor: ");
+	factorInput = createInput(31, "number");
 
-    let domHeight = 5 + input.height + 5;
+	let domHeight = 5 + input.height + 5;
 
-    factorText.position(5, domHeight);
-    factorInput.position(5 + factorText.width + 10, domHeight);
+	factorText.position(5, domHeight);
+	factorInput.position(factorText.width + 20, domHeight);
 
-    domHeight += factorInput.height + 5;
+	domHeight += factorInput.height + 5;
 
-    restartButton = createButton("Restart");
-    restartButton.position(5, domHeight);
-    restartButton.mousePressed(restartChange);
+	restartButton = createButton("Restart");
+	restartButton.position(5, domHeight);
+	restartButton.mousePressed(restartChange);
 }
 
 function GetIndex(x, y, imgWidth) {
-    return (x + y * imgWidth) * 4;
+	return (x + y * imgWidth) * 4;
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+	createCanvas(windowWidth, windowHeight);
 
-    setAttributes('premultipliedAlpha', false);
+	setAttributes('premultipliedAlpha', false);
 
-    loop();
-    // noLoop();
+	loop();
+	// noLoop();
 }
 
 function draw() {
-    if (imgInput === true) {
-        // console.log(file);
+	if (imgInput === true) {
+		// console.log(file);
 
-        if (imgIn) {
-            console.log("----- IMAGE IN -----");
+		if (imgIn) {
+			console.log("----- IMAGE IN -----");
 
-            steps = width;
+			steps = width;
 
-            let domHeight = height + 5;
+			let domHeight = height + 5;
 
-            input.position(5, domHeight);
+			input.position(5, domHeight);
 
-            domHeight += input.height + 5;
+			domHeight += input.height + 5;
 
-            factorText.position(5, domHeight);
-            factorInput.position(5 + factorText.width + 10, domHeight);
+			factorText.position(5, domHeight);
+			factorInput.position(factorText.width + 20, domHeight);
 
-            domHeight += factorInput.height + 5;
-            restartButton.position(5, domHeight);
+			domHeight += factorInput.height + 5;
+			restartButton.position(5, domHeight);
 
-            factor = factorInput.value();
+			factor = factorInput.value();
 
-            loadPixels();
+			loadPixels();
 
-            // img.loadPixels(pixels);
-            if (!restarted) {
-                img.loadPixels(pixels);
-            } else {
-                restarted = false;
-            }
+			// img.loadPixels(pixels);
+			if (!restarted) {
+				img.loadPixels(pixels);
+			} else {
+				restarted = false;
+			}
 
-            for (let x_i = 0; x_i < width; x_i++) {
-                for (let y_i = 0; y_i < height; y_i++) {
-                    const index = img.index(x_i, y_i);
+			for (let x_i = 0; x_i < width; x_i++) {
+				for (let y_i = 0; y_i < height; y_i++) {
+					const index = img.index(x_i, y_i);
 
-                    for (let i = 0; i < 4; i++) {
-                        let data = img.forOutput(index + i);
-                        data = Dither.bayerSingle(x_i, y_i, data, factor);
+					for (let i = 0; i < 4; i++) {
+						let data = img.forOutput(index + i);
+						data = Dither.bayerSingle(x_i, y_i, data, factor);
 
-                        pixels[index + i] = data * 255;
-                    }
-                }
-            }
+						pixels[index + i] = data * 255;
+					}
+				}
+			}
 
-            updatePixels();
+			updatePixels();
 
-            process = true;
-            x = 0;
-            y = 0;
-        } else {
-            alert("Try choosing image again");
-        }
+			process = true;
+			x = 0;
+			y = 0;
+		} else {
+			alert("Try choosing image again");
+		}
 
-        imgInput = false;
-    } else if (process && !imgInput) {
-        loadPixels();
-        for (let step = 0; step < steps; step++) {
-            const index = GetIndex(x, y, width);
+		imgInput = false;
+	} else if (process && !imgInput) {
+		loadPixels();
+		for (let step = 0; step < steps; step++) {
+			const index = GetIndex(x, y, width);
 
-            let col = LinearACES.ToneMap(
-                img.data[index + 0],
-                img.data[index + 1],
-                img.data[index + 2]
-            );
+			let col = LinearACES.ToneMap(
+				img.data[index + 0],
+				img.data[index + 1],
+				img.data[index + 2]
+			);
 
-            for (let i = 0; i < 3; i++) {
-                if (i < 3) {
-                    col[i] = sRGB.toSRGB(col[i]);
-                }
+			for (let i = 0; i < 3; i++) {
+				if (i < 3) {
+					col[i] = sRGB.toSRGB(col[i]);
+				}
 
-                col[i] = Dither.bayerSingle(x, y, col[i], factor);
+				col[i] = Dither.bayerSingle(x, y, col[i], factor);
 
-                pixels[index + i] = col[i] * 255;
-            }
+				pixels[index + i] = col[i] * 255;
+			}
 
-            pixels[index + 3] = Dither.bayerSingle(x, y, img.data[index + 3], factor) * 255;
+			pixels[index + 3] = Dither.bayerSingle(x, y, img.data[index + 3], factor) * 255;
 
-            x++;
-            if (x >= width) {
-                x = 0;
-                y++;
-            }
+			x++;
+			if (x >= width) {
+				x = 0;
+				y++;
+			}
 
-            if (y >= height) {
-                console.log("----- PROCESS -----");
+			if (y >= height) {
+				console.log("----- PROCESS -----");
 
-                process = false;
+				process = false;
 
-                console.log(pixels);
-                console.log(img);
-                break;
-            }
-        }
+				console.log(pixels);
+				console.log(img);
+				break;
+			}
+		}
 
-        updatePixels();
-    }
+		updatePixels();
+	}
 }
 
 function handleFile(f) {
-    // console.log(f);
-    if (f.type == 'image') {
-        fileImg = createImg(f.data, '', 'anonymous', imgReadSuccess);
+	// console.log(f);
+	if (f.type == 'image') {
+		fileImg = createImg(f.data, '', 'anonymous', imgReadSuccess);
 
-        fileImg.hide();
-    }
+		fileImg.hide();
+	}
 }
 
 function imgReadSuccess() {
-    resizeCanvas(fileImg.width, fileImg.height);
+	resizeCanvas(fileImg.width, fileImg.height);
 
-    img = new ImgWrap(width, height, ColorSpace.sRGB);
+	img = new ImgWrap(width, height, ColorSpace.sRGB);
 
-    background(28, 28, 28, 0);
-    image(fileImg, 0, 0);
+	background(28, 28, 28, 0);
+	image(fileImg, 0, 0);
 
-    loadPixels();
-    console.log(pixels);
-    updatePixels();
+	loadPixels();
+	console.log(pixels);
+	updatePixels();
 
-    imgInput = true;
-    process = false;
-    imgIn = true;
-    restarted = false;
+	imgInput = true;
+	process = false;
+	imgIn = true;
+	restarted = false;
 }
 
 function restartChange() {
-    console.log("restart");
+	console.log("restart");
 
-    if (imgIn) {
-        imgInput = true;
-        process = false;
-        restarted = true;
+	if (imgIn) {
+		imgInput = true;
+		process = false;
+		restarted = true;
 
-        background(0, 0, 0, 0);
-    }
+		background(0, 0, 0, 0);
+	}
 }
